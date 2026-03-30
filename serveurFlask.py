12ef5c7191cbf2b0c400568be85412edb920992d
quentinallegri-cmd <quentin.allegri@gmail.com>
2026-03-30 13:38:58 +0200

    index.html et script python pour affichage web

---
+++ serveurFlask.py
@@ -0,0 +1,21 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+# Route 1 : Affiche l'interface graphique (HTML + JS)
+@app.route('/')
+def index():
+	return render_template('index.html') # Envoie le fichier HTML
+
+# Route 2 : Renvoie uniquement le chiffre (Donnée brute)
+@app.route('/data')
+def get_data():
+	try:
+    	# Lecture du driver dans le kernelspace [cite: 537, 638, 648]
+    	fd = os.open("/dev/ultrason", os.O_RDONLY)
+    	distance = os.read(fd, 10).decode().strip()
+    	os.close(fd)
+    	return distance # Renvoie juste "15.4" par exemple
+	except:
+    	return "Erreur"
