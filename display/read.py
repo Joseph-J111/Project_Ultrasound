1104783bc102c30b9eaa6fc66979efa007f906ff
Agoapper <aurelie.goapper@insa-rennes.fr>
2026-03-30 16:57:45 +0200

    Update read.py - better

--- display/read.py
+++ display/read.py
@@ -1,4 +1,5 @@
 import struct
+import time
 
  #---------------- begin_function -----------------------------
 
@@ -16,46 +17,17 @@ def read_data(file_path) :
         return f"Erreur d'ouverture "
     finally :
         f.close()
-        
-def update_IHM() :
-    res=read_data(fichier_test)
-
-    if res is None :
-        label_distance.config(text="Données incomplètes", fg="orange")
-    elif res == "Erreur d'ouverture":
-        label_distance.config(text="Fichier introuvable", fg="red")
-    else:
-        # On met à jour le texte du label avec la valeur
-        label_distance.config(text=f"{res:.2f} cm", fg="black")
-    
-    # CRITIQUE : On replanifie la mise à jour dans 500ms
-    # On passe le NOM de la fonction 'update_gui', on ne l'appelle pas avec ()
-    root.after(500, update_IHM)
-
-
-
 
 
 #-------------------- end_function -------------------------
         #test de la fonction dans terminal
 fichier_test = "dev/test_binaire.bin"    #chemin à modifier 
-resultat = read_data(fichier_test)
-print(f"Lecture du fichier : {fichier_test} ")
-print(f"Valeur trouvée : {resultat} cm")
+while True : 
+ resultat = read_data(fichier_test)
+ print(f"Lecture du fichier : {fichier_test} ")
+ print(f"Valeur trouvée : {resultat} cm")
+ print(f"               Ctrl + C pour stopper ^^")
+ time.sleep(1)
 
-        # test de la fonction dans IHM
-import tkinter as tk
-
-#create the main window
-root=tk.Tk()  #prend la librairie de tkinter
-root.title("Résultat")
-root.geometry("300x200")  #taille de la fenêtre
-
-# Create a label to print the distance
-label_distance = tk.Label(root, text="En attente ..")
-label_distance.pack(pady=30)
-
-root.after(100, update_IHM)  #appel de la fonction après 100ms pour éviter de bloquer l'affichage de la fenêtre
-root.mainloop()   #permet d'afficher la fenêtre et de la maintenir ouverte quand on lance IHM
 
 
